dd1d7f2413d6360ec1c24fd86543a04056305cc0
Jakub <malinowski.jakub@yahoo.com>
2022-03-03 16:08:09 -0600

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record){
+    const win = record.find(record => record.result === 'W');
+    console.log (win);
+        if (win){
+     return (`${win.year}`);
+    }
+}
